64b5f9875e7c682f4c4d9f17639150422560d09b
ZoeyF75 <zf.zoodle@gmail.com>
2021-07-16 16:45:09 -0700

    list item component and styling

--- ShoppingList/App.js
+++ ShoppingList/App.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet, FlatList} from 'react-native';
 import { v4 as uuid } from 'uuid';
 
 import Header from './components/Header';
-
+import ListItem from './components/ListItem';
 
 const App = () => {
   const [items, setItems] = useState([
@@ -29,7 +29,7 @@ const App = () => {
       <Header />
       <FlatList 
         data={items}  
-        renderItem={({item})=> <Text>{item.text}</Text>}
+        renderItem={({item})=> <ListItem item={item} />}
       />
     </View>
   );
